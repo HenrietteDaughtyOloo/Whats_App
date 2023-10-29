462ef6e73106ace1f402865a92458c20c4725053
HenrietteDaughtyOloo <henriettedoloo@gmail.com>
2023-10-29 04:58:41 +0300

    Created the Authentication using firebase

---
+++ app/src/main/java/com/henriette/whatsappclone/activity/OTPActivity.kt
@@ -0,0 +1,103 @@
+package com.henriette.whatsappclone.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.Firebase
+import com.google.firebase.FirebaseException
+import com.google.firebase.appcheck.appCheck
+import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
+import com.google.firebase.auth.PhoneAuthProvider.OnVerificationStateChangedCallbacks
+import com.google.firebase.initialize
+import com.henriette.whatsappclone.R
+import com.henriette.whatsappclone.databinding.ActivityOtpactivityBinding
+import java.util.concurrent.TimeUnit
+
+class OTPActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityOtpactivityBinding
+    private lateinit var auth: FirebaseAuth
+    private lateinit var  verificationId:String
+    private lateinit var dialog:AlertDialog
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityOtpactivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage("This may Take a while, PLease Wait...")
+        builder.setTitle("Loading")
+        builder.setCancelable(false)
+
+        dialog = builder.create()
+        dialog.show()
+
+        val phoneNumber = "+254" + intent.getStringExtra("number")
+
+
+//        Log.d("SHUBH", "onCreate": "+phoneNumber")
+        val options = PhoneAuthOptions.newBuilder(auth)
+            .setPhoneNumber(phoneNumber)
+            .setTimeout(60L, TimeUnit.SECONDS)
+            .setActivity(this)
+            .setCallbacks(object : OnVerificationStateChangedCallbacks() {
+                override fun onVerificationCompleted(p0: PhoneAuthCredential) {
+                }
+
+                override fun onVerificationFailed(p0: FirebaseException) {
+                    dialog.dismiss()
+                    Toast.makeText(this@OTPActivity, "OOpps, Please Try Again!! ${p0}", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onCodeSent(p0: String, p1: PhoneAuthProvider.ForceResendingToken) {
+                    super.onCodeSent(p0, p1)
+                    dialog.dismiss()
+                    verificationId = p0
+                }
+            }).build()
+
+        PhoneAuthProvider.verifyPhoneNumber(options)
+
+        binding.btnContinueButton.setOnClickListener {
+            dialog.show()
+            if (binding.tilOtpNumber.text!!.isEmpty()) {
+                Toast.makeText(this, "Please Enter OTP", Toast.LENGTH_SHORT).show()
+            } else {
+                dialog.show()
+                val credential = PhoneAuthProvider.getCredential(
+                    verificationId!!,
+                    binding.tilOtpNumber.text.toString()
+                )
+
+                auth.signInWithCredential(credential)
+                    .addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            dialog.dismiss()
+                            startActivity(Intent(this, ProfileActivity::class.java))
+                            finish()
+
+                        } else {
+                            dialog.dismiss()
+                            Toast.makeText(this, "Error${it.exception}", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            }
+
+
+        }
+        Firebase.initialize(context = this)
+        Firebase.appCheck.installAppCheckProviderFactory(
+            PlayIntegrityAppCheckProviderFactory.getInstance(),
+        )
+
+    }
+}
---
+++ app/src/main/java/com/henriette/whatsappclone/activity/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.henriette.whatsappclone.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.henriette.whatsappclone.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
